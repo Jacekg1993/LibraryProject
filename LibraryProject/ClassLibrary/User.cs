073ca7c4736a25eb0e4e7e092a1845ba7add12a9
jacekg93 <jacekg93@gmail.com>
2020-05-03 15:56:15 +0200

    Created members of User class

--- LibraryProject/ClassLibrary/User.cs
+++ LibraryProject/ClassLibrary/User.cs
@@ -4,7 +4,58 @@ using System.Text;
 
 namespace ClassLibrary
 {
-    class Usercs
-    {
+    public class User
+    {       
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        private string surName;
+        public string SurName
+        {
+            get
+            {
+                return this.surName;
+            }
+            private set
+            {
+                this.surName = value;
+            }
+        }
+
+        private float penalty;
+        public float Penalty
+        {
+            get
+            {
+                return this.penalty;
+            }
+            private set
+            {
+                this.penalty = value;
+            }
+        }
+
+        List<Borrowing> UsersBorrowings = new List<Borrowing>();
+
+
+
+
+
+        private class Borrowing
+        {
+            private ushort bookID;
+
+
+        }
     }
 }
